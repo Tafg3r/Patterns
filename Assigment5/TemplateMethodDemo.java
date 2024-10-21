91828cbc8be18a2ea8cc61aa740f4275cdf8442a
Tale <dimamyzeka1999@gmail.com>
2024-10-21 18:36:53 +0500

    Assignment5

---
+++ Assigment5/TemplateMethodDemo.java
@@ -0,0 +1,52 @@
+package Assigment1.Assigment5;
+
+abstract class Game {
+    // Template method
+    public final void play() {
+        initialize();
+        startPlay();
+        endPlay();
+    }
+
+    abstract void initialize();
+    abstract void startPlay();
+    abstract void endPlay();
+}
+
+class Cricket extends Game {
+    void initialize() {
+        System.out.println("Cricket Game Initialized! Start playing.");
+    }
+
+    void startPlay() {
+        System.out.println("Cricket Game Started. Enjoy the game!");
+    }
+
+    void endPlay() {
+        System.out.println("Cricket Game Finished!");
+    }
+}
+
+class Football extends Game {
+    void initialize() {
+        System.out.println("Football Game Initialized! Start playing.");
+    }
+
+    void startPlay() {
+        System.out.println("Football Game Started. Enjoy the game!");
+    }
+
+    void endPlay() {
+        System.out.println("Football Game Finished!");
+    }
+}
+
+public class TemplateMethodDemo {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
